e47b6c9e923b523a409a0c642ff39ff8cce1de52
Ryan Huynh <ryanhuynh@mwc-074017.dhcp.missouri.edu>
2021-08-28 17:47:42 -0500

    print end of semester date

--- site.js
+++ site.js
@@ -4,11 +4,14 @@ function calculateDailySpendRate() {
     var input = document.getElementById("frm1");
     var res = parseFloat(input.elements[0].value); //Convert the values to floats and print them
     var text = res.toString();
+
+    today = new Date(); //Keeps track of today's date
+    var endOfSemester = new Date(today.getFullYear,11,17); //Keeps track of the end of the semester
       
-    n = new Date();
+    n = endOfSemester
     y = n.getFullYear();
     m = n.getMonth() + 1;
     d = n.getDate();
 
-    document.getElementById("today").innerHTML = m + "/" + d + "/" + y; 
+    document.getElementById("today").innerHTML = "The semester ends on " + m + "/" + d + "/" + y; 
 }
