aad62e1172c61b1d0eaa3d835390fa86aaea2b35
Ryan Huynh <ryanhuynh@mwc-074017.dhcp.missouri.edu>
2021-08-28 17:28:39 -0500

    add date

--- site.js
+++ site.js
@@ -1,19 +1,19 @@
 //(function( site, undefined ) {
 
-    function calculateDailySpendRate() {
-        var input = document.getElementById("frm1");
-        var res = parseFloat(input.elements[0].value); //Convert the values to floats and print them
-        var text = res.toString();
+function calculateDailySpendRate() {
+    var input = document.getElementById("frm1");
+    var res = parseFloat(input.elements[0].value); //Convert the values to floats and print them
+    var text = res.toString();
       
-        n = new Date();
-        y = n.getFullYear();
-        m = n.getMonth() + 1;
-        d = n.getDate();
+    n = new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
 
-        document.getElementById("today").innerHTML = m + "/" + d + "/" + y;
-      
-    };
+    document.getElementById("today").innerHTML = m + "/" + d + "/" + y; 
+}
 
+/*
     timeout = 400;
 
     site.partypartyparty = function(e) {
@@ -38,4 +38,4 @@
 //}( window.site = window.site || {} ));
 
 //site.partypartyparty();
-
+*/
