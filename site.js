0a410152d4e5be71bb5412eb155910e85bab1d1c
Ryan Huynh <ryanhuynh@mwc-074017.dhcp.missouri.edu>
2021-08-28 17:35:50 -0500

    fit mobile screens

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
-//(function( site, undefined ) {
 
+//This function is called when the user presses the 'Calculate' button
 function calculateDailySpendRate() {
     var input = document.getElementById("frm1");
     var res = parseFloat(input.elements[0].value); //Convert the values to floats and print them
@@ -12,30 +12,3 @@ function calculateDailySpendRate() {
 
     document.getElementById("today").innerHTML = m + "/" + d + "/" + y; 
 }
-
-/*
-    timeout = 400;
-
-    site.partypartyparty = function(e) {
-	var partyers = document.getElementsByClassName('party');
-	for(var i=0; i<partyers.length; i++) {
-            var r = 0;
-            var g = 0;
-            var b = 0;
-            var e = partyers[i];
-	    e.style.setProperty('color', 'rgb('+ r +','+ g +',' + b + ')');
-
-	    if (Math.random()*100 > 99) {
-		e.innerText = 'PARTY! PARTY! PARTY!';
-	    }
-	    
-	    this.timeout = Math.max(this.timeout * 0.9, 50);
-	}
-
-        window.setTimeout(site.partypartyparty, this.timeout);
-    };
-
-//}( window.site = window.site || {} ));
-
-//site.partypartyparty();
-*/
