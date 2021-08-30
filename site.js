cf52f966e7cf0b6d46aa70e432120de27a2d762a
Ryan Huynh <ryanhuynh@mwc-074035.dhcp.missouri.edu>
2021-08-29 19:24:30 -0500

    add last day of the semester before the button

--- site.js
+++ site.js
@@ -1,18 +1,19 @@
+//This function is called when the site loads
+function onload() {
+    const endOfSemesterDate = new Date(2021,11,17); //Keeps track of the end of the semester
+
+//Store the last day of the semester
+    const y = endOfSemesterDate.getFullYear();
+    const m = endOfSemesterDate.getMonth() + 1;
+    const d = endOfSemesterDate.getDate();
+
+//Print the last day of the semester    
+    document.getElementById("endOfSemester").innerHTML = "The semester ends on " + m + "/" + d + "/" + y; 
+}
 
 //This function is called when the user presses the 'Calculate' button
 function calculateDailySpendRate() {
     var input = document.getElementById("frm1");
     var res = parseFloat(input.elements[0].value); //Convert the values to floats and print them
     var text = res.toString();
-
-    const endOfSemester = new Date(2021,11,17); //Keeps track of the end of the semester
-
-//Store the last day of the semester
-    const y = endOfSemester.getFullYear();
-    const m = endOfSemester.getMonth() + 1;
-    const d = endOfSemester.getDate();
-
-//Print the last day of the semester    
-//    document.getElementById("today").innerHTML = endOfSemester;
-    document.getElementById("today").innerHTML = "The semester ends on " + m + "/" + d + "/" + y; 
 }
